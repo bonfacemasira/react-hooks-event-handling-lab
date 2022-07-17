a3d2dee4c8c6244e04ec4963fdf7dbed560ce51e
Bonface Masira <bonfacemasira@gmail.com>
2022-07-17 23:39:15 +0300

    commit tree 16ff37b6c89a9a3243315ba352d4574b44f366e8

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,17 @@
 // Code EyesOnMe Component Here
+export default function EyesOnMe() {
+
+        function hanldeFocus(event) {
+            console.log('Good!')
+        }
+
+        function handleBlur(event) {
+            console.log('Hey! Eyes on me!')
+        }
+
+    return (
+        <div>
+            <button onFocus={hanldeFocus} onBlur={handleBlur}>Eyes on me</button>
+        </div>
+    )
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,14 @@
 // Code Keypad Component Here
+export default function Keypad() {
+
+    function handleChange(event) {
+        console.log('Entering password...')
+    }
+
+
+    return (
+        <div>
+            <input type="password" onChange={handleChange} />
+        </div>
+    )
+}
